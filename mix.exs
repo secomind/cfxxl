5c47b22eaa5f650b288f8aa70ca2a7f5215acd08
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-19 16:05:27 +0200

    mix.exs: add poison dep

--- mix.exs
+++ mix.exs
@@ -28,6 +28,8 @@ defmodule CFXXL.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.13"}]
+    [{:httpoison, "~> 0.13"},
+     {:poison, "~> 3.1"}
+    ]
   end
 end
