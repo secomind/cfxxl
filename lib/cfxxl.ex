097533826f5756efe4f459f0122d6c001f7198f9
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-19 15:53:32 +0200

    Initial commit
    Create project with
    mix new cfxxl --module CFXXL

---
+++ lib/cfxxl.ex
@@ -0,0 +1,18 @@
+defmodule CFXXL do
+  @moduledoc """
+  Documentation for CFXXL.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CFXXL.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
