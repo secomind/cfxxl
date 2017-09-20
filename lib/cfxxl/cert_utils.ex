6f7c7705e392bcf33653b167cfdbd63d20531749
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-20 16:31:12 +0200

    cert_utils: add authority_key_identifier! function

--- lib/cfxxl/cert_utils.ex
+++ lib/cfxxl/cert_utils.ex
@@ -9,6 +9,8 @@ defmodule CFXXL.CertUtils do
 
   Record.defrecordp :certificate, :Certificate, Record.extract(:Certificate, from_lib: "public_key/include/public_key.hrl")
   Record.defrecordp :tbs_certificate, :TBSCertificate, Record.extract(:TBSCertificate, from_lib: "public_key/include/public_key.hrl")
+  Record.defrecordp :extension, :Extension, Record.extract(:Extension, from_lib: "public_key/include/public_key.hrl")
+  Record.defrecordp :authority_key_identifier, :AuthorityKeyIdentifier, Record.extract(:AuthorityKeyIdentifier, from_lib: "public_key/include/public_key.hrl")
 
   @doc """
   Extracts the serial number of a certificate.
@@ -22,6 +24,31 @@ defmodule CFXXL.CertUtils do
     |> tbs()
     |> tbs_certificate(:serialNumber)
     |> to_string()
+  end
+
+  @doc """
+  Extracts the authority key identifier of a certificate.
+
+  `cert` must be a string containing a PEM encoded certificate.
+
+  Returns the authority key identifier as string or raises if
+  it doesn't find one or there's an error.
+  """
+  def authority_key_identifier!(cert) do
+    extensions = cert
+      |> tbs()
+      |> tbs_certificate(:extensions)
+      |> Enum.map(fn(x) -> extension(x) end)
+
+    case Enum.find(extensions, fn(ext) -> ext[:extnID] == @aki_oid end) do
+      nil ->
+        raise "no AuthorityKeyIdentifier in certificate"
+
+      aki_extension ->
+        :public_key.der_decode(:AuthorityKeyIdentifier, aki_extension[:extnValue])
+        |> authority_key_identifier(:keyIdentifier)
+        |> Base.encode16(case: :lower)
+    end
   end
 
   defp tbs(cert) do
