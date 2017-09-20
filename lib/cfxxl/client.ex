41d4d06b79e4ef6ce9401b73edef431e46cc5b12
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-20 10:18:00 +0200

    client.ex: add trailing slash to endpoint

--- lib/cfxxl/client.ex
+++ lib/cfxxl/client.ex
@@ -1,7 +1,15 @@
 defmodule CFXXL.Client do
-  defstruct endpoint: "http://localhost:8888"
+  defstruct endpoint: "http://localhost:8888/"
 
   def new(), do: %__MODULE__{}
-  def new(endpoint), do: %__MODULE__{endpoint: endpoint}
+
+  def new(endpoint) do
+    slashed_endpoint = if String.ends_with?(endpoint, "/") do
+        endpoint
+      else
+        endpoint <> "/"
+      end
+    %__MODULE__{endpoint: slashed_endpoint}
+  end
 
 end
