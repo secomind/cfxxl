eee49c614dc21f3212807fb8d277e268625a90b0
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-20 10:11:40 +0200

    cfxxl: remove placeholder code

--- lib/cfxxl.ex
+++ lib/cfxxl.ex
@@ -2,17 +2,4 @@ defmodule CFXXL do
   @moduledoc """
   Documentation for CFXXL.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> CFXXL.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
