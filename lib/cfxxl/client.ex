f3102e2bb378ba75dab0adea5b3e96fdd4ac1e0c
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-20 11:28:11 +0200

    client: append api_prefix to endpoint

--- lib/cfxxl/client.ex
+++ lib/cfxxl/client.ex
@@ -1,15 +1,17 @@
 defmodule CFXXL.Client do
-  defstruct endpoint: "http://localhost:8888/"
+  @api_prefix "api/v1/cfssl/"
+
+  defstruct endpoint: "http://localhost:8888/#{@api_prefix}"
 
   def new(), do: %__MODULE__{}
 
-  def new(endpoint) do
-    slashed_endpoint = if String.ends_with?(endpoint, "/") do
-        endpoint
+  def new(base_url) do
+    endpoint = if String.ends_with?(base_url, "/") do
+        "#{base_url}#{@api_prefix}"
       else
-        endpoint <> "/"
+        "#{base_url}/#{@api_prefix}"
       end
-    %__MODULE__{endpoint: slashed_endpoint}
+    %__MODULE__{endpoint: endpoint}
   end
 
 end
