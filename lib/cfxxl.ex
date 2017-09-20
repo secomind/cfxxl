1d62182d48353d62db98f96ebd36dbee8db9e549
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-20 12:16:55 +0200

    cfxxl: add basic sign function
    Don't use options for now

--- lib/cfxxl.ex
+++ lib/cfxxl.ex
@@ -2,4 +2,26 @@ defmodule CFXXL do
   @moduledoc """
   Documentation for CFXXL.
   """
+
+  alias CFXXL.Client
+
+  def sign(%Client{endpoint: endpoint}, csr, _options \\ []) do
+    body = %{certificate_request: csr}
+
+    HTTPoison.post("#{endpoint}/sign", Poison.encode!(body))
+    |> process_response()
+  end
+
+  defp process_response({:error, _} = response), do: response
+  defp process_response({:ok, %HTTPoison.Response{body: body}}), do: extract_result(body)
+
+  defp extract_result(body) do
+    case Poison.decode(body) do
+      {:error, _} -> {:error, :invalid_response}
+      {:ok, %{"success" => false} = decoded} -> {:error, extract_errors(decoded)}
+      {:ok, %{"success" => true, "result" => result}} -> {:ok, result}
+    end
+  end
+
+  defp extract_errors(%{"errors" => errors}), do: {errors}
 end
