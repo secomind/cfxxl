1ba2483227df7e79a24e837639d02b4b3f4afb37
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-20 15:06:47 +0200

    Add CFXXL.CertUtils module
    Extract the serial number from a certificate

---
+++ lib/cfxxl/cert_utils.ex
@@ -0,0 +1,27 @@
+defmodule CFXXL.CertUtils do
+  @moduledoc """
+  A module containing utility functions to extract informations from PEM certificates
+  """
+
+  require Record
+
+  Record.defrecordp :certificate, :Certificate, Record.extract(:Certificate, from_lib: "public_key/include/public_key.hrl")
+  Record.defrecordp :tbs_certificate, :TBSCertificate, Record.extract(:TBSCertificate, from_lib: "public_key/include/public_key.hrl")
+
+  @doc """
+  Extracts the serial number of a certificate.
+
+  `cert` must be a string containing a PEM encoded certificate.
+
+  Returns the serial number as string or raises if there's an error.
+  """
+  def serial_number!(cert) do
+    cert
+    |> :public_key.pem_decode()
+    |> hd()
+    |> :public_key.pem_entry_decode()
+    |> certificate(:tbsCertificate)
+    |> tbs_certificate(:serialNumber)
+    |> to_string()
+  end
+end
