6bf14ac39a3bcfd966330c2fd8104204f7c46e98
Riccardo Binetti <riccardo.binetti@ispirata.com>
2017-09-21 18:05:24 +0200

    client: add options to struct and new function
    Use it to pass options to HTTPoison

--- lib/cfxxl/client.ex
+++ lib/cfxxl/client.ex
@@ -1,17 +1,17 @@
 defmodule CFXXL.Client do
   @api_prefix "api/v1/cfssl"
 
-  defstruct endpoint: "http://localhost:8888/#{@api_prefix}"
+  defstruct endpoint: "http://localhost:8888/#{@api_prefix}", options: []
 
   def new(), do: %__MODULE__{}
 
-  def new(base_url) do
+  def new(base_url, options \\ []) do
     endpoint = if String.ends_with?(base_url, "/") do
         "#{base_url}#{@api_prefix}"
       else
         "#{base_url}/#{@api_prefix}"
       end
-    %__MODULE__{endpoint: endpoint}
+    %__MODULE__{endpoint: endpoint, options: options}
   end
 
 end
