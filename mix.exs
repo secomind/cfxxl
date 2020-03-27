30bfb800b4a313d3cb7828416862350d2f3893f5
Davide Bettio <davide.bettio@ispirata.com>
2020-03-27 13:18:23 +0100

    Update minimum required Elixir version to 1.7
    httpoison requires at least Elixir 1.7.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CFXXL.Mixfile do
       app: :cfxxl,
       description: "CFSSL API client for Elixir",
       version: "1.0.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.7",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
